15c00cba18c9b2efd5bc3fe1d91d909e5743e367
luojiongtian <luojiongtian@sina.com>
2020-07-06 16:26:49 +0800

    flutter国际化示例代码初始化

---
+++ lib/darkmode/dark_mode_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/generated/l10n.dart';
+import 'package:flutterapp/darkmode/dark_mode_provider.dart';
+import 'package:provider/provider.dart';
+
+class DarkModePage extends StatefulWidget {
+  @override
+  _DarkModePageState createState() => _DarkModePageState();
+}
+
+class _DarkModePageState extends State<DarkModePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(S.of(context).darkMode),
+        ),
+        body: Center(
+            child: ListView(
+          children: ListTile.divideTiles(context: context, tiles: [
+            ListTile(
+              title: Text(S.of(context).lightMode),
+              onTap: () {
+                Provider.of<DarkModeProvider>(context, listen: false)
+                    .changeMode(0);
+              },
+            ),
+            ListTile(
+              title: Text(S.of(context).darkMode),
+              onTap: () {
+                Provider.of<DarkModeProvider>(context, listen: false)
+                    .changeMode(1);
+              },
+            ),
+            ListTile(
+              title: Text(S.of(context).automatic),
+              onTap: () {
+                Provider.of<DarkModeProvider>(context, listen: false)
+                    .changeMode(2);
+              },
+            )
+          ]).toList(),
+        )));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutterapp/darkmode/dark_mode_provider.dart';
+import 'package:flutterapp/generated/l10n.dart';
+import 'package:flutterapp/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: DarkModeProvider()),
+      ],
+      child: Consumer<DarkModeProvider>(
+        builder: (context, darkModeProvider, _) {
+          return darkModeProvider.darkMode == 2
+              ? MaterialApp(
+                  // 设置语言
+                  localizationsDelegates: const [
+                    S.delegate,
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate
+                  ],
+                  // 讲zh设置为第一项,没有适配语言时，英语为首选项
+                  supportedLocales: S.delegate.supportedLocales,
+                  title: 'Flutter Demo',
+                  theme: ThemeData(
+                    primarySwatch: Colors.blue,
+                  ),
+                  darkTheme: ThemeData.dark(),
+                  home: MyHomePage(title: 'Flutter Challenge Demo'),
+                )
+              : MaterialApp(
+                  // 设置语言
+                  localizationsDelegates: const [
+                    S.delegate,
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate
+                  ],
+                  // 讲zh设置为第一项,没有适配语言时，英语为首选项
+                  supportedLocales: S.delegate.supportedLocales,
+                  title: 'Flutter Demo',
+                  theme: darkModeProvider.darkMode == 1
+                      ? ThemeData.dark()
+                      : ThemeData(
+                          primarySwatch: Colors.blue,
+                        ),
+                  home: MyHomePage(title: 'Flutter Challenge Demo'),
+                );
+        },
+      ),
+    );
+  }
+}
